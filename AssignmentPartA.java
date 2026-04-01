4581dbdb7a42f61cebb1ca651c11eb4c1ac59bf4
Zaid Imam <nasirzaid333@gmail.com>
2026-04-01 11:49:51 +0500

    AssignmentPartA.java

--- AssignmentPartA.java
+++ AssignmentPartA.java
@@ -1,23 +1,26 @@
-
-    import java.util.Scanner;
+import java.util.Scanner;
 
 public class AssignmentPartA {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter an integer: ");
-        int num = sc.nextInt();
+
+        Scanner input = new Scanner(System.in);
+          
+        System.out.println("Enter integer");
+        int number = input.nextInt();
+        
 
         String binary = "";
-        int temp = num;
+        while ( number > 0) {
+            int remainder = number % 2;
+            binary = remainder + binary;
 
-        while (temp > 0) {
-            binary = (temp % 2) + binary;  // prepend remainder
-            temp = temp / 2;
+            number=number/2;
         }
-
-        if (binary.isEmpty()) binary = "0";
-
-        System.out.println(num + " in binary = " + binary);
+        System.out.println("binary ="+ binary);
+        
+      
     }
 }
+
+   
 
